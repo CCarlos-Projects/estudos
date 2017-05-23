a00ebd13437bb12c0eec7f7d6382eda006c22808
JCristo <JCristo@DESKTOP-FPTDCM8>
2017-05-23 21:03:25 +0100

    Estudos
    Estudos

---
+++ Estudos/Controllers/GoogleMApsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Estudos.Controllers
+{
+    public class GoogleMapsController : Controller
+    {
+        // GET: GoogleMAps
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
