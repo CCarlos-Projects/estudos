c8bf3b09315685269f5f7f85b3221426c50f4d3d
JCristo <JCristo@DESKTOP-FPTDCM8>
2017-05-23 21:28:31 +0100

    Detect Browser

---
+++ Estudos/Controllers/MobileController.cs
@@ -0,0 +1,17 @@
+﻿using Estudos.Models.Methods;
+using System.Web.Mvc;
+
+namespace Estudos.Controllers
+{
+    public class MobileController : Controller
+    {
+        // GET: Mobile
+        public ActionResult Index()
+        {
+            if (DetectBrowsers.IsBrowserMobile())
+                return View("Mobile-Index");
+
+            return View();
+        }
+    }
+}
